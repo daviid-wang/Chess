0dea4f6e926b0a01fcb6e6c8cd3d6e36fa80b5ef
David <david.holt2002@gmail.com>
2023-11-19 13:27:53 +0800

    basic chess board + position storage

--- chess.py
+++ chess.py
@@ -1,5 +1,19 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-my_window = tk.Tk()
+ROW_NUM = 8
+COL_NUM = 8
 
-my_window.mainloop()
+position = [[0]*ROW_NUM]*COL_NUM
+
+def initial_position():
+    print(position)
+
+initial_position()
+
+root = Tk()
+frm = ttk.Frame(root, padding=300)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+# root.mainloop()

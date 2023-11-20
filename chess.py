c197b45c47ab6e0d3e24f6c1cbe96ccb073c5dfe
David <david.holt2002@gmail.com>
2023-11-20 13:56:17 +0800

    Initial positions set

--- chess.py
+++ chess.py
@@ -1,12 +1,64 @@
+from enum import Enum
 from tkinter import *
 from tkinter import ttk
 
 ROW_NUM = 8
 COL_NUM = 8
 
-position = [[0]*ROW_NUM]*COL_NUM
+position = [[0 for i in range(8)] for i in range(8)]
+
+class WhitePiece(Enum):
+    ROOK    = "R"
+    KNIGHT  = "N"
+    BISHOP  = "B"
+    QUEEN   = "Q"
+    KING    = "K"
+    PAWN    = "P"
+
+class BlackPiece(Enum):
+    ROOK    = "R"
+    KNIGHT  = "N"
+    BISHOP  = "B"
+    QUEEN   = "Q"
+    KING    = "K"
+    PAWN    = "P"
 
 def initial_position():
+    position[0][0] = BlackPiece.ROOK
+    position[0][1] = BlackPiece.KNIGHT
+    position[0][2] = BlackPiece.BISHOP
+    position[0][3] = BlackPiece.QUEEN
+    position[0][4] = BlackPiece.KING
+    position[0][5] = BlackPiece.BISHOP
+    position[0][6] = BlackPiece.KNIGHT
+    position[0][7] = BlackPiece.ROOK
+    
+    position[1][0] = BlackPiece.PAWN
+    position[1][1] = BlackPiece.PAWN
+    position[1][2] = BlackPiece.PAWN
+    position[1][3] = BlackPiece.PAWN
+    position[1][4] = BlackPiece.PAWN
+    position[1][5] = BlackPiece.PAWN
+    position[1][6] = BlackPiece.PAWN
+    position[1][7] = BlackPiece.PAWN
+
+    position[6][0] = WhitePiece.PAWN
+    position[6][1] = WhitePiece.PAWN
+    position[6][2] = WhitePiece.PAWN
+    position[6][3] = WhitePiece.PAWN
+    position[6][4] = WhitePiece.PAWN
+    position[6][5] = WhitePiece.PAWN
+    position[6][6] = WhitePiece.PAWN
+    position[6][7] = WhitePiece.PAWN
+
+    position[7][0] = WhitePiece.ROOK
+    position[7][1] = WhitePiece.KNIGHT
+    position[7][2] = WhitePiece.BISHOP
+    position[7][3] = WhitePiece.QUEEN
+    position[7][4] = WhitePiece.KING
+    position[7][5] = WhitePiece.BISHOP
+    position[7][6] = WhitePiece.KNIGHT
+    position[7][7] = WhitePiece.ROOK
     print(position)
 
 initial_position()
@@ -16,4 +68,4 @@ frm = ttk.Frame(root, padding=300)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-# root.mainloop()
+root.mainloop()
